3fee58b38c6302c360eb368ec4d50ef78ad10aad
YunusErdemGokdag <120821444+panheadd@users.noreply.github.com>
2026-04-22 20:54:34 +0300

    pw field , no hash rn

--- HealthandFitnessTrackerAPI/Models/Trainer.cs
+++ HealthandFitnessTrackerAPI/Models/Trainer.cs
@@ -7,6 +7,7 @@ public class Trainer
     [FirestoreProperty] public string Id { get; set; } = Guid.NewGuid().ToString();
     [FirestoreProperty] public string Name { get; set; }
     [FirestoreProperty] public string Email { get; set; }
+    [FirestoreProperty] public string Password { get; set; }
 
     [FirestoreProperty] public List<string> ClientIds { get; set; } = new();
 }
--- HealthandFitnessTrackerAPI/Models/User.cs
+++ HealthandFitnessTrackerAPI/Models/User.cs
@@ -7,6 +7,7 @@ public class User
     [FirestoreProperty] public string Id { get; set; } = Guid.NewGuid().ToString();
     [FirestoreProperty] public string Username { get; set; }
     [FirestoreProperty] public string Email { get; set; }
+    [FirestoreProperty] public string Password { get; set; }
     [FirestoreProperty] public int Age { get; set; }
 
     [FirestoreProperty] public List<string> TrainerIds { get; set; } = new();
